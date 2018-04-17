0e30b38acf40cf572e665b6aa4852f803e502a4e
Bonney <bonney@berkeley.edu>
2018-04-16 18:59:09 -0700

    grey blue wave

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-  
+
   $(".chaffle").chaffle();
   $('#typed').typed({
-    strings: ['designer','visual communicator', 'pizza enthusiast', 'typography junkie'],
+    strings: ['designer.','visual communicator.', 'pizza enthusiast.', 'typography junkie.'],
     showCursor: true,
     typeSpeed: 40,
     backSpeed: 20,
     shuffle: true,
     startDelay: 1000,
-    backDelay: 1000, 
-    loop: true,  
+    backDelay: 1000,
+    loop: true,
   });
-  
+
   // $(".link").click(function() {
   //   $('html,body').animate({
   //       scrollTop: $(".work").offset().top},
