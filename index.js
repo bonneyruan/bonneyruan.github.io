4ee34199764bd2cfbfee2c3aeda0fd18c6d9586b
Bonn3y <Bonn3y@Bonneys-MacBook-Pro.local>
2018-11-10 12:40:54 -0800

    side bar animated

---
+++ index.js
@@ -0,0 +1,9 @@
+function sidebar_open() {
+    document.getElementById("sidebar").style.display = "block";
+}
+function sidebar_close() {
+    document.getElementById("sidebar").style.display = "none";
+}
+
+
+
