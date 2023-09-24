d25141975bc99f5377a1f46b1229db86b62ff7df
bonney <bonney@berkeley.edu>
2023-09-24 13:20:11 -0700

    mill ends park base pages

--- millendsparkJS.js
+++ millendsparkJS.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
   if(sessionStorage.getItem('save') != '1' && sessionStorage.getItem('save') != '2'){
-    alert('empty');
-    sessionStorage.setItem('save', '1');
+    sessionStorage.setItem('save', '2');
   }
   if(sessionStorage.getItem('save') == '1'){
     $('.mini-site-container').toggleClass("zoomed");
@@ -15,9 +14,7 @@ $(document).ready(function(){
         return v === 'zoom in' ? 'zoom out' : 'zoom in'
     })
     let val = $('.mini-site-container').hasClass("zoomed") ? '1' : '2';
-    console.log(val);
     sessionStorage.setItem('save', val);
-    alert(sessionStorage.getItem('save'));
     });
 
   $('#spotpark').click(function() {
@@ -33,6 +30,53 @@ $('#spotbg').click(function(event) {
     $('#tryagain').removeClass("tryagainfade");
     void $('#tryagain').width();
     $('#tryagain').addClass("tryagainfade");
+  }
+  });
+
+var score = 0;
+$('#millendsbutton').click(function() {
+  if($('#doorbutton').hasClass("hidden")){
+    $('#scoretext').text(score+" out of 3");
+    $('#scoreresults').removeClass("hidden");
+  }
+  $('#incorrect').removeClass("hidden");
+  $('#nextbutton').removeClass("hidden");
+  $('.otherlabel').addClass("hidden");
+  });
+
+$('#whalebutton').click(function() {
+  $('#correct').removeClass("hidden");
+  $('#nextbutton').removeClass("hidden");
+  $('#millendslabel').addClass("hidden");
+  score += 1;
+  });
+
+$('#doorbutton').click(function() {
+  $('#correct').removeClass("hidden");
+  $('#nextbutton').removeClass("hidden");
+  $('#millendslabel').addClass("hidden");
+  score += 1;
+  });
+
+$('#greatdanebutton').click(function() {
+  $('#correct').removeClass("hidden");
+  $('#nextbutton').removeClass("hidden");
+  $('#millendslabel').addClass("hidden");
+  score += 1;
+  $('#scoretext').text(score+" out of 3");
+  $('#scoreresults').removeClass("hidden");
+  });
+
+$('#nextbutton').click(function() {
+  $('#incorrect').addClass("hidden");
+  $('#correct').addClass("hidden");
+  $('.otherlabel').removeClass("hidden");
+  $('#millendslabel').removeClass("hidden");
+  if($('#whalebutton').hasClass("hidden") && $('#doorbutton').not("hidden")){
+    $('#doorbutton').addClass("hidden");
+  }
+  if($('#whalebutton').not("hidden")){
+    $('#whalebutton').addClass("hidden");
   }
   });
 });
